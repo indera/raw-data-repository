4c6acd0395bd0d6e5798584b21e94ce3f22e60cc
Dan Rodney <danqrodney>
2017-04-28 08:48:04 -0500

    Changes

--- rest-api/tools/setup_monitoring.sh
+++ rest-api/tools/setup_monitoring.sh
@@ -5,13 +5,13 @@
 # Example usage:
 # tools/setup_monitoring.sh --account dan.rodney@pmi-ops.org --project all-of-us-rdr-staging [--update]
 
-CREATE_INSTANCE=
+EXTRA_ARGS=
 USAGE="tools/setup_monitoring.sh --account <ACCOUNT> --project <PROJECT> [--update]"
 while true; do
   case "$1" in
     --account) ACCOUNT=$2; shift 2;;
     --project) PROJECT=$2; shift 2;;
-    --update) UPDATE=Y; shift 1;;
+    --update) EXTRA_ARGS="--update"; shift 1;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -23,17 +23,23 @@ then
   exit 1
 fi
 
+POLICY_FILE=config/alerts/$PROJECT.json
+
 if [ -z "${ACCOUNT}" ]
 then
   echo "--account is required. $USAGE"
   exit 1
 fi
 
+if [ -z "${CREDS_ACCOUNT}" ]
+then
+  CREDS_ACCOUNT="${ACCOUNT}"
+fi
+
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 export PYTHONPATH=$PYTHONPATH:${BASE_DIR}:${BASE_DIR}/lib
 
-gcloud auth login $ACCOUNT
-gcloud config set project $PROJECT
-gcloud auth application-default login
+source tools/auth_setup.sh
 
-(cd ${BASE_DIR}; python tools/monitoring_client.py --project $PROJECT)
+(export GOOGLE_APPLICATION_CREDENTIALS="${CREDS_FILE}"; cd ${BASE_DIR};
+ python tools/monitoring_client.py --project $PROJECT --policy_file $POLICY_FILE $EXTRA_ARGS)
