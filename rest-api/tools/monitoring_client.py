a94e40fdf18fd3dba226f55c821d98ca73c99e52
Dan Rodney <danrodney@google.com>
2017-05-22 11:37:53 -0500

    Delint

--- rest-api/tools/monitoring_client.py
+++ rest-api/tools/monitoring_client.py
@@ -4,28 +4,29 @@ import logging
 import os
 import sys
 from oauth2client.client import GoogleCredentials
-from oauth2client import client
 from googleapiclient.discovery import build
 
 _USER_METRIC_FILTER = 'metric.type = starts_with("logging.googleapis.com/user/")'
 _POLICIES_KEY = 'policies'
 _METRIC_DESCRIPTORS_KEY = 'metricDescriptors'
 
-PROJECT_MAP = {'pmi-drc-api-test': { 'api_key': 'AIzaSyCAuQdK6L5AU7c1EOhkeJwEw-7oCs5HsiE',
-                                     'project_id': '118972441757',
-                                     'notification_channel_ids': ['841940086686265891', '7271756574530877070'] },
-            'all-of-us-rdr-staging': { 'api_key': 'AIzaSyB3A9zGsvc9DPCdTXleXIs9wHRIRUbfA-E',
-                                       'project_id': '261005263653',
-                                       'notification_channel_ids': ['1726500439316347724'] },
-            'all-of-us-rdr-dryrun': { 'api_key': 'AIzaSyDWsIYuhfLO5CWnTuiw1Bm8m5aSaR8kQQ0',
-                                      'project_id': '90017737200',
-                                      'notification_channel_ids': ['515929249894770147'] },
-            'all-of-us-rdr-stable': { 'api_key': 'AIzaSyBmLhyFlg2q_vxllk27R-3t5ZNzJTf0C40',
-                                      'project_id': '812931298053',
-                                      'notification_channel_ids': [] },
-            'all-of-us-rdr-prod': { 'api_key': 'AIzaSyAUcQj4l-8DfqS5-A_InN7VD8ZN_lLtflU',
-                                    'project_id': '106251944765',
-                                    'notification_channel_ids': ['771610197803600435', '13127179291565379182']}}
+PROJECT_MAP = {'pmi-drc-api-test': {'api_key': 'AIzaSyCAuQdK6L5AU7c1EOhkeJwEw-7oCs5HsiE',
+                                    'project_id': '118972441757',
+                                    'notification_channel_ids': ['841940086686265891',
+                                                                 '7271756574530877070']},
+            'all-of-us-rdr-staging': {'api_key': 'AIzaSyB3A9zGsvc9DPCdTXleXIs9wHRIRUbfA-E',
+                                      'project_id': '261005263653',
+                                      'notification_channel_ids': ['1726500439316347724']},
+            'all-of-us-rdr-dryrun': {'api_key': 'AIzaSyDWsIYuhfLO5CWnTuiw1Bm8m5aSaR8kQQ0',
+                                     'project_id': '90017737200',
+                                     'notification_channel_ids': ['515929249894770147']},
+            'all-of-us-rdr-stable': {'api_key': 'AIzaSyBmLhyFlg2q_vxllk27R-3t5ZNzJTf0C40',
+                                     'project_id': '812931298053',
+                                     'notification_channel_ids': []},
+            'all-of-us-rdr-prod': {'api_key': 'AIzaSyAUcQj4l-8DfqS5-A_InN7VD8ZN_lLtflU',
+                                   'project_id': '106251944765',
+                                   'notification_channel_ids': ['771610197803600435',
+                                                                '13127179291565379182']}}
 
 def create_monitoring_client(api_key, credentials):
   discovery_base_url = 'https://monitoring.googleapis.com/$discovery/rest'
@@ -73,11 +74,13 @@ def write_policies(policy_file, policies, metric_descriptors):
   with open(policy_file, 'w') as fp:
     policy_json = {_POLICIES_KEY: policies, _METRIC_DESCRIPTORS_KEY: metric_descriptors}
     json.dump(policy_json, fp, indent=4)
-  logging.info('Wrote %d policies and %d metric descriptors to %s.' % (len(policies), len(metric_descriptors),
+  logging.info('Wrote %d policies and %d metric descriptors to %s.' % (len(policies),
+                                                                       len(metric_descriptors),
                                                                        policy_file))
 
 def create_metric_descriptor(metric_descriptors_api, project_id, metric_descriptor):
-  result = metric_descriptors_api.create(name=make_name(project_id), body=metric_descriptor).execute()
+  result = metric_descriptors_api.create(name=make_name(project_id),
+                                         body=metric_descriptor).execute()
   logging.info('Created metric_descriptor: %s' % result)
 
 def main(args):
@@ -100,7 +103,8 @@ def main(args):
   local_metric_descriptors = policy_json.get(_METRIC_DESCRIPTORS_KEY) or []
   if args.update:
     existing_policy_map = {policy['name']: policy for policy in existing_policies}
-    existing_descriptor_map = {descriptor['name']: descriptor for descriptor in existing_metric_descriptors}
+    existing_descriptor_map = {descriptor['name']: descriptor
+                               for descriptor in existing_metric_descriptors}
 
     for policy in local_policies:
       policy_name = policy.get('name')
@@ -129,7 +133,8 @@ def main(args):
         if existing_descriptor == metric_descriptor:
           logging.info('Descriptor %s remains unchanged.', descriptor_name)
         else:
-          logging.warning('Descriptor %s differs in content (%s vs %s); rename to make new descriptor.',
+          logging.warning('Descriptor %s differs in content (%s vs %s); ' +
+                          'rename to make new descriptor.',
                           descriptor_name, metric_descriptor, existing_descriptor)
     existing_policies = list(list_policies(policies_api, project_id))
     existing_metric_descriptors = list(list_metric_descriptors(metric_descriptors_api, project_id))
